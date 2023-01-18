40d11fbe3e974c1fadb2322d705553d1948b4847
krd4 <k000ryousuke@gmail.com>
2023-01-18 21:20:55 +0900

    add morgan

--- src/web/server.ts
+++ src/web/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 
 const port = 3000;
 const app = express();
+
+app.use(morgan("short"))
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}.`)
